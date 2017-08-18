fd85908f61b4b8d9dccd10bed6d065f0246f5e5b
wtran29 <wtran29@gmail.com>
2017-08-18 08:26:58 -0700

    updated models

--- apps/user_app/models.py
+++ apps/user_app/models.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
-
+import datetime
+import bcrypt, re
+EMAIL_REGEX=re.compile(r'^[a-zA-Z0-9+._-]+@[a-zA-Z0-9+._-]+\.[a-zA-Z]+$')
+NAME_REGEX=re.compile(r'^[A-Za-z]\w+$')
 # Create your models here.
+
+class UserManager(models.Manager):
+	def RegValid(self, postData):
+		errors ={}
+
+
+class User(models.Model):
+	email= models.EmailField(unique=True)
+
+
+	objects = UserManager()
+	
+	def __str__(self):
+		return self.email
